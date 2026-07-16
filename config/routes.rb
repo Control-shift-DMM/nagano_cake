8a20e0217f726744dbd8912f0ec6f567a1a775c6
hayato005560-crypto <hayato005560@gmail.com>
2026-07-16 00:38:47 +0000

    Fix route lint

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,18 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: "homes#top"
 
-    resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres, only: [:index, :create, :edit, :update]
-    resources :customers, only: [:index, :show, :edit, :update]
-    resources :orders, only: [:show, :update]
-    resources :order_details, only: [:update]
+    resources :items, only: [ :index, :new, :create, :show, :edit, :update ]
+    resources :genres, only: [ :index, :create, :edit, :update ]
+    resources :customers, only: [ :index, :show, :edit, :update ]
+    resources :orders, only: [ :show, :update ]
+    resources :order_details, only: [ :update ]
   end
 
   scope module: :public do
     root to: "homes#top"
     get "about", to: "homes#about"
 
-    resources :items, only: [:index, :show]
+    resources :items, only: [ :index, :show ]
 
     get "customers/my_page", to: "customers#show"
     get "customers/information/edit", to: "customers#edit"
@@ -23,16 +23,17 @@ Rails.application.routes.draw do
     get "customers/unsubscribe", to: "customers#unsubscribe"
     patch "customers/withdraw", to: "customers#withdraw"
 
-    resources :cart_items, only: [:index, :create, :update, :destroy] do
+    resources :cart_items, only: [ :index, :create, :update, :destroy ] do
       delete :destroy_all, on: :collection
     end
 
-    resources :orders, only: [:new, :create, :index, :show] do
+    resources :orders, only: [ :new, :create, :index, :show ] do
       post :confirm, on: :collection
       get :complete, on: :collection
     end
 
-    resources :addresses, only: [:index, :edit, :create, :update, :destroy]
+    resources :addresses, only: [ :index, :edit, :create, :update, :destroy ]
   end
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
