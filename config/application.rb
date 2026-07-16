efed0c8644bd7075eb9bd2237d02aa37da93195c
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-16 06:01:03 +0000

    [update]config/application.rb スペル修正 locate → locale

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module WorkGithub
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    config.i18n.default_locate = :ja
+    config.i18n.default_locale = :ja
 
   end
 end
