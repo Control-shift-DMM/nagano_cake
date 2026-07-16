fd3ad256d68032caf7fb433780c3d774a55ae185
Ubuntu <ubuntu@ip-172-31-6-142.ap-northeast-1.compute.internal>
2026-07-16 16:43:04 +0000

    配送先のコントローラ作成とビューの編集途中

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,5 +1,9 @@
 class Public::AddressesController < ApplicationController
+  before_action :authenticate_customer!
+
   def index
+    @address = Address.new
+    @addresses = current_customer.addresses
   end
 
   def edit
