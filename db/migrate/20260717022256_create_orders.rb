ceb8aa4377484dce12e3bfa81cb1023e62d00ca3
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-17 12:47:30 +0000

    [修正]マイグレーションファイル内スペルミス修正・[反映]db:migration 実施

--- db/migrate/20260717022256_create_orders.rb
+++ db/migrate/20260717022256_create_orders.rb
@@ -3,13 +3,13 @@ class CreateOrders < ActiveRecord::Migration[8.1]
     create_table :orders do |t|
       t.references :customer, null: false, foreign_key: true
       t.string :name, null: false
-      t.strign :postal_code , null: false
+      t.string :postal_code , null: false
       t.string :address , null:false
       t.integer :payment_method , null: false
       t.integer :shipping_cost , null: false
       t.integer :total_payment, null: false
       t.integer :status , null: false , default: 0
-      t.timestamps , null: false
+      t.timestamps null: false
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_16_044934) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_17_022256) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -88,7 +88,22 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_16_044934) do
     t.index ["genre_id"], name: "index_items_on_genre_id"
   end
 
+  create_table "orders", force: :cascade do |t|
+    t.string "address", null: false
+    t.datetime "created_at", null: false
+    t.integer "customer_id", null: false
+    t.string "name", null: false
+    t.integer "payment_method", null: false
+    t.string "postal_code", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "status", default: 0, null: false
+    t.integer "total_payment", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "genres"
+  add_foreign_key "orders", "customers"
 end
