fc70cbe80a0befc0ed7f4d7c53ede63d40122f72
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-17 03:03:08 +0000

    [update]order.rbにコメント追記

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :customer
 
 
-  #enum 設定
+  #enum 設定 config/ja.ymlに設定済み
   enum :payment_method, { credit_card: 0, transfer: 1 }
   enum :status, { waiting_for_payment: 0, payment_confirmation: 1 ,in_production:2, preparation:3, shipping:4}
 end
