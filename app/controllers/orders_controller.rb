8708f297e29579e53efa86ce8ebe822346b08591
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-17 13:05:43 +0000

    [進行中]注文情報入力画面・注文情報確認画面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
   def new
+    @order = Current.customer.orders.new
+    # ログイン顧客が登録してる配送先のレコード全取得
+    @addresses = Current.customer.addresses
   end
 
   def confirm
+    @order = Current.customer.orders.new(order_params)
+    set_order
   end
 
   def complete
@@ -15,5 +20,38 @@ class OrdersController < ApplicationController
   end
 
   def show
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(
+      :payment_method,
+      :postal_code,
+      :address,
+      :name,
+      :select_address,
+      :address_id
+    )
+  end
+
+  # 郵便番号・住所・宛名をセットする処理
+  def set_order
+    case params[:order][:select_address]
+    when "0"  #ご自身の住所の場合 値の上書き
+      @order.postal_code = Current.customer.postal_code
+      @order.address     = Current.customer.address
+      @order.name        = "#{Current.customer.last_name}#{Current.customer.first_name}"
+
+    when "1" #登録済み住所の場合 値の上書き
+      address = Current.customer.addresses.find(params[:order][:address_id])
+
+      @order.postal_code = address.postal_code
+      @order.address     = address.address
+      @order.name        = address.name
+
+    when "2" #新しいお届け先の場合
+      # order_paramsで既に代入済み
+    end
+
   end
 end
