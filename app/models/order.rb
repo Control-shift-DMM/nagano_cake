15eec2609c70b61189d85756549c87f0652e5921
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-20 08:08:17 +0000

    lintエラー修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
   has_many :items, through: :order_details
 
 
-  #enum 設定
+  # enum 設定
   enum :payment_method, { credit_card: 0, transfer: 1 }
-  enum :status, { waiting_for_payment: 0, payment_confirmation: 1 ,in_production:2, preparation:3, shipping:4}
+  enum :status, { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparation: 3, shipping: 4 }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,5 @@ class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
 
-  enum :making_status, { discontinued: 0, waiting_for_creation: 1, in_production:2, complete:3 }
+  enum :making_status, { discontinued: 0, waiting_for_creation: 1, in_production: 2, complete: 3 }
 end
