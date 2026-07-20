2eee55cc4b4fdb9d5db230fc3fc002280f2a6da7
Ubuntu <ubuntu@ip-172-31-6-142.ap-northeast-1.compute.internal>
2026-07-20 13:44:30 +0000

    developの変更と自分の変更を統合

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,6 +1,9 @@
 class Public::AddressesController < ApplicationController
+  # Deviseの標準認証（後で戻す可能性があるので念のため残す）
   # before_action :authenticate_customer!
 
+  before_action :require_customer_authentication
+  
   def index
     @address = Address.new
     @addresses = current_customer.addresses
