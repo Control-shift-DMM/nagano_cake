2b4c5ed7bdeb3c8a59472651ba2664e4ce786316
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-20 05:20:58 +0000

    db:migrate反映 テーブル作成ファイル以外に悪影響及ぼすものは削除or空白に変更

--- db/migrate/20260719041630_create_order_details.rb
+++ db/migrate/20260719041630_create_order_details.rb
@@ -3,9 +3,9 @@ class CreateOrderDetails < ActiveRecord::Migration[8.1]
     create_table :order_details do |t|
       t.references :item, null: false, foreign_key: true
       t.references :order, null: false, foreign_key: true
-      t.integer :price ,null: false
-      t.integer :amount ,null: false
-      t.integer :making_status , null: false , default: 0
+      t.integer :price, null: false
+      t.integer :amount, null: false
+      t.integer :making_status, null: false, default: 0
       t.timestamps
     end
   end
