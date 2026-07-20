382158715a38487f8126a34ba7f619cae5444798
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-20 06:31:52 +0000

    テストファイル試作中・インデント・スペルミス修正

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,10 +1,10 @@
-class OrdersController < ApplicationController
+class Public::OrdersController < ApplicationController
   before_action :set_order_and_cart_items ,only: [:confirm ,:create]
   before_action :set_payment_info ,only: [:confirm ,:create]
   def new
-    @order = Current.customer.orders.new
+    @order = Current.account.orders.new
     # ログイン顧客が登録してる配送先のレコード全取得
-    @addresses = Current.customer.addresses
+    @addresses = Current.account.addresses
   end
 
   def confirm
@@ -27,23 +27,25 @@ class OrdersController < ApplicationController
 
       @cart_items.destroy_all
     end
+
     redirect_to complete_orders_path
 
-    # save・create処理で例外が発生したロールバックしこの処理を実行
-    rescue ActiveRecord::RecordInvalid
-      # データ作成失敗時リダイレクト
-      redirect_to new_order_path
-    end
+  # save・create処理で例外が発生した場合値をロールバックしこの処理を実行(トランザクション処理)
+  # インデントはdef createと同じ位置が正常位置
+  rescue ActiveRecord::RecordInvalid
+    # データ作成失敗時リダイレクト
+    redirect_to new_order_path
   end
 
+
   def index
-    @orders = Current.customer.orders
+    @orders = Current.account.orders
                               .includes(order_details: :item)
                               .order(created_at: :desc)
   end
 
   def show
-    @order = Current.customer.orders
+    @order = Current.account.orders
                              .includes(order_details: :item)
                              .find(params[:id])
   end
@@ -61,8 +63,8 @@ class OrdersController < ApplicationController
   end
 
   def set_order_and_cart_items
-    @order = Current.customer.orders.new(order_params)
-    @cart_items = Current.customer.cart_items.includes(:item)
+    @order = Current.account.orders.new(order_params)
+    @cart_items = Current.account.cart_items.includes(:item)
   end
 
   def set_payment_info
@@ -74,12 +76,12 @@ class OrdersController < ApplicationController
   def set_order_element
     case params[:order][:select_address]
     when "0"  #ご自身の住所の場合 値の上書き
-      @order.postal_code = Current.customer.postal_code
-      @order.address     = Current.customer.address
-      @order.name        = "#{Current.customer.last_name}#{Current.customer.first_name}"
+      @order.postal_code = Current.account.postal_code
+      @order.address     = Current.account.address
+      @order.name        = "#{Current.account.last_name}#{Current.account.first_name}"
 
     when "1" #登録済み住所の場合 値の上書き
-      address = Current.customer.addresses.find(params[:order][:address_id])
+      address = Current.account.addresses.find(params[:order][:address_id])
 
       @order.postal_code = address.postal_code
       @order.address     = address.address
