d308da0e3ed8e91ca480f14e7c67d27d6a1ec15f
Ubuntu <ubuntu@ip-172-31-6-142.ap-northeast-1.compute.internal>
2026-07-21 14:22:25 +0000

    注文機能で使うメゾット追加・ci/testエラー解消

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -3,4 +3,9 @@ class CartItem < ApplicationRecord
   belongs_to :item
 
   validates :amount, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
+
+  def calc_sub_total
+    price * amount
+  end
+
 end
