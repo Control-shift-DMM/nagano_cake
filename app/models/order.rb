536b83d3fd36fdcdd9eb51c8654bbf48174c84bc
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-21 05:22:44 +0000

    Orderモデルバリデーションチェック設定追記

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,20 @@ class Order < ApplicationRecord
   # enum 設定
   enum :payment_method, { credit_card: 0, transfer: 1 }
   enum :status, { waiting_for_payment: 0, payment_confirmation: 1, in_production: 2, preparation: 3, shipping: 4 }
+
+  validates :payment_method, presence: true
+
+  validates :address_id,
+            presence: true,
+            if: -> { select_address == "1" }
+
+  validates :postal_code,
+            :address,
+            :name,
+            presence: true,
+            if: -> { select_address == "2" }
+
+  validates :postal_code,
+            format: { with: /\A\d{7}\z/ },
+            allow_blank: true
 end
