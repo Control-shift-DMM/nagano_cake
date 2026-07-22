68995716eb2c16e7537fd443116450635cd77bec
mr-sayaka <289574848+mr-sayaka@users.noreply.github.com>
2026-07-22 07:48:39 +0000

    会員マイページ機能を追加

---
+++ app/controllers/public/customers_controller.rb
@@ -0,0 +1,21 @@
+class Public::CustomersController < ApplicationController
+   def show
+    @customer= Current.account
+   end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(
+      :last_name,
+      :first_name,
+      :last_name_kana,
+      :first_name_kana,
+      :postal_code,
+      :address,
+      :telephone_number,
+      :email,
+      :is_active
+    )
+  end
+end
