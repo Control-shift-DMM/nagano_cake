b1cbbf7d3ae9e4595788399ba764214300021ea9
Ubuntu <ubuntu@ip-172-31-6-142.ap-northeast-1.compute.internal>
2026-07-22 09:02:24 +0000

    カート内商品一覧画面編集

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,16 +1,33 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = current_customer.cart_items
+    @total_price = 0 #合計金額　あとで調節
   end
 
   def update
+    # 数量変更の処理
   end
 
   def destroy
+    # 1件削除の処理
   end
 
   def destroy_all
+    # 全削除の処理
   end
 
   def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if @cart_item.save
+      redirect_to cart_items_path, notice: 'カートに商品を追加しました。'
+    else
+      redirect_to items_path, alert: 'カートに商品を追加できませんでした。'
+    end
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
