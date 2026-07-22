627c913e457c438a84ce9588836a1ef4b4c6e447
Ubuntu <ubuntu@ip-172-31-15-27.ap-northeast-1.compute.internal>
2026-07-22 08:00:05 +0000

    顧客側商品一覧・商品詳細

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,12 +1,11 @@
 class Public::ItemsController < ApplicationController
   def index
-    @items = Item
-             .includes(:genre)
-             .where(is_active: true)
-             .order(:id)
+    @items = Item.where(is_active: true)
   end
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+    @genres = Genre.all
   end
 end
