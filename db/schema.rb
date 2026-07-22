7957a2bf1d788c1eb0b3fbb173322316987cd576
TanakaDaiki-57413 <274158570+TanakaDaiki-57413@users.noreply.github.com>
2026-07-22 05:01:07 +0000

    db:migrate実施

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_07_21_063949) do
+ActiveRecord::Schema[8.1].define(version: 2026_07_21_085614) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -136,7 +136,10 @@ ActiveRecord::Schema[8.1].define(version: 2026_07_21_063949) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "addresses", "customers"
+  add_foreign_key "addresses", "customers"
   add_foreign_key "cart_items", "customers"
+  add_foreign_key "cart_items", "customers"
+  add_foreign_key "cart_items", "items"
   add_foreign_key "cart_items", "items"
   add_foreign_key "items", "genres"
   add_foreign_key "order_details", "items"
