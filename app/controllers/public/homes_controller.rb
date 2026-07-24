442477d1c41a5e45276358f7f0b362e13b203b60
Ubuntu <ubuntu@ip-172-31-15-27.ap-northeast-1.compute.internal>
2026-07-24 06:32:22 +0000

    トップ画面のジャンル一覧とレイアウトを修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,10 @@
 class Public::HomesController < ApplicationController
   def top
+    @items = Item.where(is_active: true).order(created_at: :desc).limit(4)
+    @genres = Genre.all
   end
 
   def about
+    @genres = Genre.all
   end
 end
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,9 +4,13 @@ class Public::ItemsController < ApplicationController
              .includes(:genre)
              .where(is_active: true)
              .order(:id)
+
+             @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
+    @cart_item = CartItem.new
+    @genres = Genre.all
   end
 end
